f06ab0f75faedddf9231e643dc924d713d40c9a6
David Del Val <ddelvalfraire@outlook.com>
2024-10-03 14:40:13 -0700

    add builtin "len" method for object.String

--- object/object.go
+++ object/object.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+type BuiltinFunction func(args ...Object) Object
+
 type ObjectType string
 
 type Object interface {
@@ -22,6 +24,7 @@ const (
 	ERROR_OBJ        = "ERROR_OBJ"
 	FUNCTION_OBJ     = "FUNCTION_OBJ"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Integer struct {
@@ -88,3 +91,10 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
