b8ab2bf8f5b8b2cb193c61dbed186b30c35da7cb
David Del Val <ddelvalfraire@outlook.com>
2024-10-04 22:53:55 -0700

    add array indexing evaluation

--- object/object.go
+++ object/object.go
@@ -25,6 +25,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION_OBJ"
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
 
 type Integer struct {
@@ -98,3 +99,21 @@ type Builtin struct {
 
 func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
 func (b *Builtin) Inspect() string  { return "builtin function" }
+
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+
+	var elements []string
+	for _, el := range ao.Elements {
+		elements = append(elements, el.Inspect())
+	}
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+	return out.String()
+}
